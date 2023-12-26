e03f9fa3a44bc2f58a2aa01b436c1e76f0e0e0f7
happy-spark <107300540+happy-spark@users.noreply.github.com>
2023-12-26 23:53:31 +0900

    terms_array 값 할당 반복문 오류 수정 및 결과 표시

--- script.js
+++ script.js
@@ -2,9 +2,17 @@ document.getElementById("calc-btn").onclick = function () {
     let IntV = document.getElementById("int-input").value.toString();
     if (IntV != "") {
         let terms_array = [];
-        for (var i = 0; i < (IntV.length/3 + IntV.length%3); i++) {
-            terms_array.push(IntV.slice(-3));
-            IntV = IntV.slice(0, IntV.slice(0, IntV.length - 3));
+        let temp_IntV = IntV;
+        for (var i = 0; i < Math.ceil(IntV.length/3); i++) {
+            terms_array.push(temp_IntV.slice(-3));
+            temp_IntV = temp_IntV.slice(0, temp_IntV.length - 3);
+        }
+        console.log(terms_array);
+        let equations = terms_array[0];
+        document.getElementById("result-div").innerHTML += equations + " = " + eval(equations);
+        for (var i = 1; i < terms_array.length; i++) {
+            equations = equations + " + " + terms_array[i];
+            document.getElementById("result-div").innerHTML += "<br>" + equations + " = " + eval(equations);
         }
     }
 };
